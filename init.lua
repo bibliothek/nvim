9a8d87a5ef6d261ce1f746037f8826f889681870
Martin Thaller <martin.thaller@uipath.com>
2024-10-15 14:33:41 +0200

    pwsh

--- init.lua
+++ init.lua
@@ -229,6 +229,11 @@ vim.opt.rtp:prepend(lazypath)
 -- Set pwsh on Windows
 if vim.fn.has("win32") then
   vim.opt.shell = "pwsh"
+  vim.opt.shellxquote=''
+  vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+  vim.opt.shellquote   = ''
+  vim.opt.shellpipe    = '| Out-File -Encoding UTF8 %s'
+  vim.opt.shellredir   = '| Out-File -Encoding UTF8 %s'
 end
 
 
