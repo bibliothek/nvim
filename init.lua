ec43267f11bbc133679c92b04843be6f982243bb
Martin Thaller <martin.thaller@uipath.com>
2025-09-15 14:59:49 +0200

    clean

--- init.lua
+++ init.lua
@@ -159,17 +159,6 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   { import = 'plugins' },
 }, {
