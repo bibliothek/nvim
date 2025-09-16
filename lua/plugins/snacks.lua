99dc0a95623b3c1c885f63833447c80306f2accd
Martin Thaller <martin.thaller@uipath.com>
2025-09-16 10:27:09 +0200

    Add git commands

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -24,8 +24,27 @@ return {
     vim.keymap.set('n', '<leader>fR', function() pick.recent({ cwd = true }) end, { desc = 'Recent (cwd)' })
 
     -- Git
-    vim.keymap.set('n', '<leader>gc', function() pick.git_log() end, { desc = 'Commits' })
-    vim.keymap.set('n', '<leader>gs', function() pick.git_status() end, { desc = 'Status' })
+    vim.keymap.set('n', '<leader>gb', function() pick.git_branches() end, { desc = 'Git Branches' })
+    vim.keymap.set('n', '<leader>gc', function() pick.git_log() end, { desc = 'Git Commits' })
+    vim.keymap.set('n', '<leader>gC', function() pick.git_log_line() end, { desc = 'Git Commits (current line)' })
+    vim.keymap.set('n', '<leader>gd', function() pick.git_diff() end, { desc = 'Git Diff' })
+    vim.keymap.set('n', '<leader>gf', function() pick.git_log_file() end, { desc = 'Git File History' })
+    vim.keymap.set('n', '<leader>gs', function() pick.git_status() end, { desc = 'Git Status' })
+    vim.keymap.set('n', '<leader>gS', function() pick.git_stash() end, { desc = 'Git Stash' })
+
+    -- Additional Git Actions
+    vim.keymap.set('n', '<leader>ga', '<cmd>Git add .<cr>', { desc = 'Git Add All' })
+    vim.keymap.set('n', '<leader>gA', '<cmd>Git add %<cr>', { desc = 'Git Add Current File' })
+    vim.keymap.set('n', '<leader>gm', '<cmd>Git commit<cr>', { desc = 'Git Commit' })
+    vim.keymap.set('n', '<leader>gM', '<cmd>Git commit --amend<cr>', { desc = 'Git Commit Amend' })
+    vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = 'Git Push' })
+    vim.keymap.set('n', '<leader>gP', '<cmd>Git pull<cr>', { desc = 'Git Pull' })
+    vim.keymap.set('n', '<leader>gL', '<cmd>Git log --oneline<cr>', { desc = 'Git Log Oneline' })
+    vim.keymap.set('n', '<leader>gl', '<cmd>Git blame<cr>', { desc = 'Git Blame' })
+    vim.keymap.set('n', '<leader>gr', '<cmd>Git reset<cr>', { desc = 'Git Reset' })
+    vim.keymap.set('n', '<leader>gR', '<cmd>Git reset --hard<cr>', { desc = 'Git Reset Hard' })
+    vim.keymap.set('n', '<leader>gw', '<cmd>Git checkout .<cr>', { desc = 'Git Restore All' })
+    vim.keymap.set('n', '<leader>gW', '<cmd>Git checkout %<cr>', { desc = 'Git Restore Current' })
 
     -- Search
     vim.keymap.set('n', '<leader>sc', function() pick.command_history() end, { desc = 'Command History' })
