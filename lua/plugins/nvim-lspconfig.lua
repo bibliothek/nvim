d0876b7ca45c8e5f4d26ee209d6fc4495e0e747f
Martin Thaller <martin.thaller@uipath.com>
2025-09-17 14:29:31 +0200

    remove-fidget

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,9 +4,6 @@ return {
     { 'williamboman/mason.nvim', config = true },
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-    { 'j-hui/fidget.nvim', opts = {} },
-
     { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
