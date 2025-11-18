349792dd3400fc3a6dd6c8778c368033b226e1e2
Martin Thaller <martin.thaller@uipath.com>
2025-11-18 17:02:53 +0100

    globals

--- init.lua
+++ init.lua
@@ -1,14 +1,4 @@
--- Set <space> as the leader key
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
---disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+require 'globals'
 
 require 'options'
 
